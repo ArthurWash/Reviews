2775c34398cca98f22b71c08319011b4c5e61170
AJ <ajw53@NJIT.edu>
2022-01-10 17:15:47 -0500

    Feature: Project complete. Added event listeners for each button, as well as fixed teh css issue.

--- app.js
+++ app.js
@@ -53,15 +53,39 @@ let currentItem = 0;
 
 // Load intial item
 window.addEventListener('DOMContentLoaded', function(){
-  showPerson(currentItem);
+  showPerson();
 });
 
 // Show person based on item
-function showPerson(person){
-  const item = reviews[person];
+function showPerson(){
+  const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
 }
 
+// Show next person
+nextBtn.addEventListener('click', function(){
+  currentItem++;
+  if(currentItem > reviews.length - 1){
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+// Show previous person
+prevBtn.addEventListener('click', function(){
+  currentItem--;
+  if(currentItem < 0){
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
+});
+
+// Surprise Me
+randomBtn.addEventListener('click', function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  console.log(currentItem);
+  showPerson()
+;});
